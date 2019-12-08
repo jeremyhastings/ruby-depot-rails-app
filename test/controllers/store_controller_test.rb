b0e8cfaa5e73277855f10872cb296e746a1a0694
Jeremy Hastings <jeremy.hastings@mac.com>
2019-12-08 09:26:44 -0800

    Added Comments

--- test/controllers/store_controller_test.rb
+++ test/controllers/store_controller_test.rb
@@ -5,7 +5,9 @@ class StoreControllerTest < ActionDispatch::IntegrationTest
     get store_index_url
     assert_response :success
     # Added extra test lines on December 8th, 2019.
+    # This test looks for a nav element with the class side_nav and that there are at least 4 present.
     assert_select 'nav.side_nav a', minimum: 4
+    # These lines verify that the products are being displayed.
     assert_select 'main ul.catalog li', 3
     assert_select 'h2', 'Programming Ruby 1.9'
     assert_select '.price', /\$[,\d]+\.\d\d/
