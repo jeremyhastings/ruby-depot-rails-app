e8b0b494084ab065c9739dfd578add317f73112d
Jeremy Hastings <jeremy.hastings@mac.com>
2019-12-08 08:13:40 -0800

    Changed root path to Store Controller Index Action.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  get 'store/index'
+
+  # Made the index action of the store controller the root page.  December 8th, 2019.
+  root 'store#index', as: 'store_index'
+
+  # Created by Scaffold for products
   resources :products
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
 end
