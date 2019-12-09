20cc51a9fc75c4fe0599862a756c540499afe504
Jeremy Hastings <jeremy.hastings@mac.com>
2019-12-09 10:33:42 -0800

    Created up method for combine_items migration and migrated db.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_09_175528) do
+ActiveRecord::Schema.define(version: 2019_12_09_182013) do
 
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
