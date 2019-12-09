4f7e333afa23041863423989fc039c35178d4905
Jeremy Hastings <jeremy.hastings@mac.com>
2019-12-09 15:32:04 -0800

    Updated Comments for Ajax Cart.

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,4 +1,5 @@
 class StoreController < ApplicationController
+  # Added Cart Module from concerns to Store Controller to make Cart visible on index action.  December 9th, 2019.
   include CurrentCart
   before_action :set_cart
 
