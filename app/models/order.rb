f3504665ce3afb43674003646d2652d7b62d158e
Jeremy Hastings <jeremy.hastings@mac.com>
2019-12-10 06:44:31 -0800

    Made changes to migration to allow cart_id to be null.

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,8 @@
 class Order < ApplicationRecord
+  # Added this prior to migration and using enum to keep track of payment type.  December 10th, 2019.
+  enum pay_type: {
+      "Check" => 0,
+      "Credit card" => 1,
+      "Purchase order" => 2
+  }
 end
