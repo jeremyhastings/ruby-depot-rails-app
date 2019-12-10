0868770dc08ca8edbd1154fd6ceade1ce4597638
Jeremy Hastings <jeremy.hastings@mac.com>
2019-12-09 17:02:40 -0800

    Added render_if method to application helper.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  # render_if partial created to determine whether cart partial should render.  December 9th, 2019.
+  # TODO: I do not understand this at all.
+  def render_if(condition, record)
+    if condition
+      render record
+    end
+  end
 end
