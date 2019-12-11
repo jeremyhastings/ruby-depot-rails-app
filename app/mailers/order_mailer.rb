0c7bee1c450484959783e6f61a572d97536af4c5
Jeremy Hastings <jeremy.hastings@mac.com>
2019-12-11 13:50:06 -0800

    Updated test mailer.  Still error.  Moving on.

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -8,7 +8,7 @@ class OrderMailer < ApplicationMailer
   def received(order)
     @order = order
 
-    mail to: order.email, subject: 'Pragmatic Store Order Confirmation'
+    mail to: order.email, from: "depot@example.org", subject: 'Pragmatic Store Order Confirmation'
   end
 
   # Subject can be set in your I18n file at config/locales/en.yml
@@ -18,6 +18,7 @@ class OrderMailer < ApplicationMailer
   #
   def shipped(order)
     @order = order
-    mail to: order.email, subject: 'Pragmatic Store Order Shipped'
+
+    mail to: order.email, from: "depot@example.org", subject: 'Pragmatic Store Order Shipped'
   end
 end
